2db2e43e8d97984a2f944bbc12bb7fae6dc0d845
MBromiley <505forensics@gmail.com>
2016-12-26 00:29:17 -0600

    Version 0.0.1

--- redsketch.py
+++ redsketch.py
@@ -8,54 +8,94 @@ __maintainer__ = "Matt Bromiley (@mbromileyDFIR)"
 __email__ = "505forensics@gmail.com"
 __status__ = "Development"
 
-''' Timesketch output format:
-message,timestamp,datetime,timestamp_desc,extra_field_1,extra_field_2
-'''
-
 import argparse
 import sqlite3
+import time
 
 def red_in(redline_file):
+    """Parse the incoming Redline data"""
     entries = []
     conn = sqlite3.connect(redline_file)
     c = conn.cursor()
-    #ID|LocalAddress|LocalPort|RemoteAddress|RemotePort|Protocol|PortState|Created|PortPID|PortProcessName|PortProcessPath|ProcessID|ItemSummaryID
-    for row in c.execute('SELECT * FROM Ports;'):
-        if row[5] == 'UDP':
-            print "UDP"
-            if row[1]:
-                entries.insert(0,"Row 1 {} Found".format(row[0]))
-            else:
-                line = "UDP Connection over port {0}, created by process {1} (PID: {2}),'','{3},Port Opened".format(row[2],row[9],row[8],row[7])
-                entries.insert(0,line)
-        elif row[5] == 'TCP':
-            print 'TCP'
+    for idx, row in enumerate(c.execute('SELECT * FROM Ports;')):
+        # Test to see if timestamp exists
+        if str(row[7]) == 'None':
+            epoch = 'None'
+            timestamp = 'None'
         else:
-            print 'Else'
+            epoch = int(time.mktime(time.strptime(str(row[7]), "%Y-%m-%d %H:%M:%SZ")))
+            timestamp = row[7]
+
+        # Test to see if process has a path, else return N/A
+        if row[10]:
+            path = row[10]
+        else:
+            path = 'N/A'
+
+        # Test for protocol; TCP vs UDP will display different options
+        if row[5] == 'UDP':
+            # Options for UDP protocol
+            if row[1]:
+                line = ["UDP Connection {0}:{1} created by process {2} (PID: {3}), " \
+                       "{4}, " \
+                       "{5}, " \
+                       "Port Opened, " \
+                       "Process Path: {6}, " \
+                       "Port State: {7}".format(row[1], row[2], row[9], row[8], epoch, timestamp, path, row[6])]
+            else:
+                line = ["UDP Connection over port {0}, " \
+                       "created by process {1} (PID: {2})," \
+                       "{3}," \
+                       "{4}," \
+                       "Port Opened," \
+                       "Process Path: {5}," \
+                       "Port State: {6}".format(row[2], row[9], row[8], epoch, timestamp, path, row[6])]
+            entries.append(line)
+
+        elif row[5] == 'TCP':
+            if row[1]:
+                line = ["TCP Connection {0}:{1} created by process {2} (PID: {3}), " \
+                       "{4}, " \
+                       "{5}, " \
+                       "Port Opened, " \
+                       "Process Path: {6}, " \
+                       "Port State: {7}".format(row[1], row[2], row[9], row[8], epoch, timestamp, path, row[6])]
+            else:
+                line = ["TCP Connection over port {0}, " \
+                       "created by process {1} (PID: {2})," \
+                       "{3}," \
+                       "{4}," \
+                       "Port Opened," \
+                       "Process Path: {5}," \
+                       "Port State: {6}".format(row[2], row[9], row[8], epoch, timestamp, path, row[6])]
+            entries.append(line)
+        else:
+            # Instead of simply failing silently, if there was an unknown protocol for
+            # _whatever_ reason, print the problem and the line
+            print 'Unknown protocol found on line {}'.format(idx)
+            print row
 
     return entries
-        #print 'Port opened between {0}:{1} and {2}:{3}'.format(row[1], row[2], row[3], row[4])
-
 def main():
+    """Main function, includes argparse, input direction, and output"""
     parser = argparse.ArgumentParser(description='Convert Mandiant Redline data into another format')
-
     # Input Options
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-f', '--file', metavar='FILE', help='MANS file to parse')
-    # group.add_argument('-d','--directory',metavar='DIR',help="Directory of MANS files")
 
-    # Output Options - future-proofing with code from other scripts
-    '''
     group = parser.add_argument_group()
-    group.add_argument('-o','--output',metavar='OUTPUT',help='Output file [default is stdout]')
-    group.add_argument('--json',action='store_true',help='Output in JSON')
-    '''
+    group.add_argument('--headers', action='store_true', help="Enable headers on output")
+
     # Parse It!
     args = parser.parse_args()
 
+    if args.headers:
+        print "Message,Timestamp,DateTime,Timestamp Description,Extra Field 1,Extra Field 2"
+
     if args.file:
         output = red_in(args.file)
-        print output
+        for row in output:
+            print row
 
 if __name__ == '__main__':
     main()
